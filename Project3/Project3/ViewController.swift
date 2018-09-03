6e327edd9a14fad784fb9fbaa2143e739d88c6d4
Johnny <Sjohnny@qiujieda.com>
2018-09-03 10:21:13 +0800

    renew the project with activity viewcontroller

--- Project3/Project3/ViewController.swift
+++ Project3/Project3/ViewController.swift
@@ -1,19 +1,62 @@
 //
 //  ViewController.swift
-//  Project3
+//  Project1
 //
-//  Created by Johnny on 2018/8/31.
+//  Created by Johnny on 2018/8/29.
 //  Copyright © 2018年 MiaSheng. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+//this is commited from xcode
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default
+        let pathString = Bundle.main.resourcePath
+        if let path = pathString {
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    // this is a picture to load!
+                    pictures.append(item)
+                    
+                }
+            }
+        }else{
+            print("There is no resource path!!!")
+        }
+        
+        print(pictures)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
