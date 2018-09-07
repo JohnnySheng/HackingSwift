58e0d86e108f7077176b2936771e1513b62b338b
Johnny <Sjohnny@qiujieda.com>
2018-09-07 10:31:32 +0800

    Add an alert view in the project 5

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
 
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt"){
+            if let startWords = try? String(contentsOfFile: startWordsPath){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }else{
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+        
+    }
+
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        //Here we define two vaiable self and ac, they are unowned in the closure,
+        //so there are no strong references, and there are no retain cycle.
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction) in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    
+    func submit(answer: String) {
+        print("Submit Answer:\(answer)")
+    }
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll()
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 
 }
 
